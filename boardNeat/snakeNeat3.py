e70ef0f0bad40a1148cd728aa4dd6cdfd47ac0ee
Neil Leonard <quantumchamploo@gmail.com>
2021-06-04 21:53:55 -0700

    more dev and tests

---
+++ boardNeat/snakeNeat3.py
@@ -0,0 +1,163 @@
+
+import random
+import os
+import time
+import neat
+import visualize
+import pickle
+from bareSnake import snakeGame
+import numpy as np
+import math
+
+gen = 0
+
+def makeList(mat):
+	hld = []
+	for i in mat:
+		for j in i:
+			#print(j)
+			hld.append(j)
+	return hld
+
+def find_index(array):
+    s = np.array(array)
+    sort_index = np.argsort(s)
+    return sort_index
+
+def eval_genomes(genomes, config):
+    print('in eval')
+
+    global gen
+
+    gen += 1
+
+
+    nets = []
+    games = []
+    ge = []
+
+    for genome_id, genome in genomes:
+        genome.fitness = 0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        games.append(snakeGame())
+        ge.append(genome)
+    run = True
+    tab = 0
+
+    while run and len(games) > 0:
+        tab +=1
+        if tab % 100 == 0:
+            print('did 100 runs')
+            print(tab)
+            print(len(games))
+
+        for x, game in enumerate(games):
+            moved = False
+            #output = nets[games.index(game)].activate((makeList(game.toString())))
+            output = nets[games.index(game)].activate((makeList(game.sight)))
+            ind_arr = find_index(output)
+            max_index = ind_arr[-1]
+            nex_index = ind_arr[-2]
+            third_index = ind_arr[-3]
+            fourth_index = ind_arr[-4]
+
+            initScore = game.score
+            if max_index == 0:
+                game.move_left()
+            if max_index == 1:
+                game.move_right()
+            if max_index == 2:
+                game.move_up()
+            if max_index == 3:
+                game.move_down()
+
+            newScore = game.score
+            # print('gene num is ')
+            # print(x)
+            # print(max_index)
+            # print(game.direction)
+            alive = game.checkGame()
+            if game.popCount > 40:
+            	alive = False
+            if alive == False:
+                nets.pop(games.index(game))
+                ge.pop(games.index(game))
+                games.pop(games.index(game))
+                #print('popped one')
+            else:
+                ge[x].fitness += 5
+                #ge[x].fitness += 4*2**(-1*game.distToFood()/15)
+                # if game.d2f < game.d2fPrev:
+                #     ge[x].fitness += 3
+                #     # if x == 0:
+                #     #     print("got closer")
+                # else:
+                #     ge[x].fitness -= 2
+                # # if x == 0:
+
+                #     print(4*2**(-1*game.distToFood()/15))
+                if newScore - initScore > 0:
+                    ge[x].fitness += 100
+                    # print (game.score)
+                    # if game.score > 1:
+                    #     for i in game.history:
+                    #         print(i)
+                    # print("ATE one!")
+            if tab % 100 == 0:
+                print('did 100 runs')
+                print(tab)
+                print(len(games))
+                print('gene num is ')
+                print(x)
+                print(max_index)
+                print(game.direction)
+                print(game.pos)
+            # if x == 0:
+            # 	print(game.toString())
+            # 	print("---")
+            # 	print("---")
+
+def run(config_file):
+    """
+    runs the NEAT algorithm to train a neural network to play flappy bird.
+    :param config_file: location of config file
+    :return: None
+    """
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+    print('post config')
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    #p.add_reporter(neat.Checkpointer(5))
+
+    # Run for up to 50 generations.
+    winner = p.run(eval_genomes,1000)
+
+    # show final stats
+    print('\nBest genome:\n{!s}'.format(winner))
+    node_names = {0:'left',1:'right',2:'up',3:'down',-1:'E body',-2:'E food',-3:'E dist'
+                                                    ,-4:'N body',-5:'N food',-6:'N dist'
+                                                    ,-7:'S body',-8:'S food',-9:'S dist'
+                                                    ,-10:'W body',-11:'W food',-12:'W dist'
+                                                    ,-13:'NE body',-14:'NE food',-15:'NE dist'
+                                                    ,-16:'SE body',-17:'SE food',-18:'SE dist'
+                                                    ,-19:'NW body',-20:'NW food',-21:'NW dist'
+                                                    ,-22:'SW body',-23:'SW food',-24:'SW dist'}
+    visualize.draw_net(config, winner, True, 'snakeMultLayer5',node_names=node_names)
+    print(repr(winner))
+
+if __name__ == '__main__':
+    print('in start')
+    # Determine path to configuration file. This path manipulation is
+    # here so that the script will run successfully regardless of the
+    # current working directory.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforwardSightSnake.txt')
+    run(config_path)
